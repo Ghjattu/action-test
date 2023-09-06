c9077f9a0d04e1f42f0ba18902d0886be6e39664
ghjattu <icracked369@gmail.com>
2023-09-06 22:08:30 +0800

    test

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDatabase() {
+	godotenv.Load("../.env")
+
+	mysql_username := os.Getenv("MYSQL_USERNAME")
+	mysql_password := os.Getenv("MYSQL_PASSWORD")
+	mysql_ip := os.Getenv("MYSQL_IP")
+	mysql_port := os.Getenv("MYSQL_PORT")
+	mysql_db_name := os.Getenv("MYSQL_DB_NAME")
+
+	dsn := mysql_username + ":" + mysql_password +
+		"@tcp(" + mysql_ip + ":" + mysql_port + ")" + "/" + mysql_db_name +
+		"?charset=utf8mb4&parseTime=True&loc=Local"
+
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+}
